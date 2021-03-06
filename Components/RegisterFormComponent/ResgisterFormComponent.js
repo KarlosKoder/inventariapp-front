fb10494c675b9cbfe07cbf7beb22217f0c5d6ff5
KarlosKoder <carlosemiliovenancio@gmail.com>
2021-03-06 13:44:45 -0600

    Login and connect with mongodb

--- Components/RegisterFormComponent/ResgisterFormComponent.js
+++ Components/RegisterFormComponent/ResgisterFormComponent.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import {Form, Button} from "react-bootstrap"
 import styles from "../../styles/register.module.scss"
-import { useRouter } from 'next/router';
+import { useState } from 'next/router';
 
+function ResgisterFormComponent(){
+    const [ userData, setUserData ] = useState({})
+    const [ logedIn , setLogedIn ] = useState(null)
+    const [ loginError, setLoginError ] = useState(false)
+
+    const changeHandler = event => {
+        setUserData( {...userData, [event.target.name] : event.target.value })
+        setLoginError( false )
+    }
+
+    const registerUser = async event => {
+        event.preventDefault()
+    
+        const res = await fetch(
+          'https://hooks.zapier.com/hooks/catch/123456/abcde',
+          {
+            body: JSON.stringify({
+              email: event.target.email.value
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            method: 'POST'
+          }
+        )
+    
+        const result = await res.json()
+        // result.user => 'Ada Lovelace'
+      }
+
+    const login = () => {
+        let response = api.login(userData)
+        console.log( response )
+        response.status === "error" ? setLoginError( response ) : bringAccess( response )
+    }
+
+    const bringAccess = ( loginData ) => {
+        console.log( loginData )
+        let { token, role } = loginData;
+        localStorage.setItem("token",token)
+        localStorage.setItem("role",role)
+        window.location.href = '/home'
+    }
     return (
         <div>
             <div style={{width:"80%", marginLeft:"10%", marginTop:"10%"}}>
@@ -20,10 +63,10 @@ import { useRouter } from 'next/router';
                     <Form.Label> Crea tu contraseña</Form.Label>
                     <Form.Control type="password" placeholder="Crea tu contraseña"/>
                 </Form.Group>
-                <Button onClick={ login } type="submit"> ¡Registrate!</Button>
+                <Button onClick={ registerUser } type="submit"> ¡Registrate!</Button>
             </form>
             
         </div>
     )
-
+}
 export default ResgisterFormComponent
