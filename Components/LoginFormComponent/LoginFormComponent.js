47640227266a506de765085d20b25a8a89edd68a
KarlosKoder <carlosemiliovenancio@gmail.com>
2021-03-09 06:29:46 -0600

    LoginFormComponent y LoginComponent agregados

--- Components/LoginFormComponent/LoginFormComponent.js
+++ Components/LoginFormComponent/LoginFormComponent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import {Form, Button} from "react-bootstrap"
 import styles from "../../styles/register.module.scss"
 
-function ResgisterFormComponent(){
+function LoginFormComponent(){
     const [ userData, setUserData ] = useState({})
     const [ logedIn , setLogedIn ] = useState(null)
     const [ loginError, setLoginError ] = useState(false)
@@ -11,29 +11,6 @@ function ResgisterFormComponent(){
         setUserData( {...userData, [event.target.name] : event.target.value })
         setLoginError( false )
     }
-
-    const registerUser = async event => {
-        let nonTextHeaders = new Headers()
-
-        event.preventDefault()
-        const res = await fetch(
-            'http://ec2-18-222-219-127.us-east-2.compute.amazonaws.com:8080/auth/signup',
-          {
-            body: JSON.stringify({
-                email : userData.inputEmail,
-                password : userData.inputPassword
-            }),
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            method: 'POST'
-          }
-        )
-    
-        const result = await res.json()
-        console.log(result)
-        
-      }
 
       const loginUser = async event => {
         let nonTextHeaders = new Headers()
@@ -76,23 +53,23 @@ function ResgisterFormComponent(){
         <div>
             <div style={{width:"80%", marginLeft:"10%", marginTop:"10%"}}>
             <h2>¿Cómo <span className={styles.spanHeader}>empezamos</span>?</h2>
-              <h5>Crea tu tienda en pocos minutos</h5>
+              <h5>Inicia sesión</h5>
 
             </div>
             
-            <form onSubmit = { registerUser } style={{width:"80%", marginLeft:"10%", marginTop:"10%"}}>
+            <form onSubmit = { loginUser } style={{width:"80%", marginLeft:"10%", marginTop:"10%"}}>
                 <Form.Group>
                     <Form.Label> Correo Electrónico</Form.Label>
-                    <Form.Control  name="inputEmail" type="email" onChange = { changeHandler } placeholder="Ingresa tu correo"/>
+                    <Form.Control  name="inputEmail" type="email" onChange = { changeHandler } placeholder="Ingresa tu correo registrado"/>
                 </Form.Group>
                 <Form.Group>
-                    <Form.Label> Crea tu contraseña</Form.Label>
-                    <Form.Control name="inputPassword" type="password" onChange = { changeHandler } placeholder="Crea tu contraseña"/>
+                    <Form.Label> Contraseña</Form.Label>
+                    <Form.Control name="inputPassword" type="password" onChange = { changeHandler } placeholder="Ingresa tu contraseña"/>
                 </Form.Group>
-                <Button type="submit"> ¡Registrate!</Button>
+                <Button type="submit"> Entrar</Button>
             </form>
             
         </div>
     )
 }
-export default ResgisterFormComponent
+export default LoginFormComponent
--- pages/login.js
+++ pages/login.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import NavComponent from "../Components/NavComponent/NavComponent";
+import Head from "next/head";
+import LoginComponent from "../Components/RegisterComponent/LoginComponent"
 
-export default {
+export const Login = () => {
+    return (
+        
+        <div>
+      <Head>
+        <title>InventariApp</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous"
+        ></link>
+      </Head>
+      <NavComponent/>
+      
+      <LoginComponent/>
 
-    login( userObject){
-        console.log(userObject)
-        let { userName, password } = userObject
+      
 
-        //!userObject && {status:"error", errorMsg:"debes proporcionar tus datos"}
+      
+      <footer className="footer">
+        <script
+          src="https://unpkg.com/react/umd/react.production.min.js"
+          crossorigin
+        ></script>
 
-        let response;
+        <script
+          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+          crossorigin
+        ></script>
 
-         userName === "israel@kodemia.mx" && password === "123456" 
-            ? response = { status: "ok", token:123456789, role:"admin"}
-            : response = { status: "error", errorCode: 300, errorMsg : "Usuario o contraseñaaa incorrectos" }
+        <script
+          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossorigin
+        ></script>
+      </footer>
+    </div>
 
-        return response
-    }
+
+    )
 }
 
-
+export default Login;
